687c0a22eedf8c24ddcb0c6bc63b8f1f915b074e
Marcel M. Cary <marcel@oak.homeunix.org>
2012-01-06 11:31:15 -0800

    Add optional progress bar to FileSource

--- lib/wolverine/file_source.rb
+++ lib/wolverine/file_source.rb
@@ -1,12 +1,34 @@
+require 'progressbar'
 module Wolverine
   class FileSource < Source
-    def initialize(filename)
+    attr_accessor :progress
+    def initialize(filename, opts={})
       @filename = filename
+      self.progress = opts[:progress]
     end
     def each
-      File.foreach(@filename) do |line|
-        yield Event.new(line)
+      progress = self.progress
+      if progress
+        size = File.size(@filename)
+        bar = ProgressBar.new(@filename, size)
+        bar.file_transfer_mode
       end
+      File.open(@filename, "r") do |file|
+        bar_update = 10_000
+        cnt = 0
+        file.each do |line|
+          if progress
+            if cnt < bar_update
+              cnt += 1
+            else
+              bar.set(file.tell)
+              cnt = 0
+            end
+          end
+          yield Event.new(line)
+        end
+      end
+      bar.finish if progress
     end
   end
 end
