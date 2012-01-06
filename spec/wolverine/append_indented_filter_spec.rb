bf0f5eab662ffb866f65f6ce8dbefc1ddccfff4b
Marcel M. Cary <marcel@oak.homeunix.org>
2012-01-06 09:21:03 -0800

    Use an event-generating source for append_indented_filter_spec, not an array

--- spec/wolverine/append_indented_filter_spec.rb
+++ spec/wolverine/append_indented_filter_spec.rb
@@ -1,18 +1,21 @@
 require File.dirname(__FILE__)+"/../spec_helper"
 describe Wolverine::AppendIndentedFilter do
   it "should append indented lines, stripping indent" do
-    filt = Wolverine::AppendIndentedFilter.new ["foo\n", "  bar"]
+    filt = Wolverine::AppendIndentedFilter.new source(["foo\n", "  bar"])
     ary = filt.map {|ev| ev.to_s}
     ary.should == ["foo\nbar"]
   end
   it "should skip initial indented lines as a partial event" do
-    filt = Wolverine::AppendIndentedFilter.new ["  foo\n", "bar\n"]
+    filt = Wolverine::AppendIndentedFilter.new source(["  foo\n", "bar\n"])
     ary = filt.map {|ev| ev.to_s}
     ary.should == ["bar\n"]
   end
   it "should output multiple events when appropriate" do
-    filt = Wolverine::AppendIndentedFilter.new ["foo\n", "bar\n"]
+    filt = Wolverine::AppendIndentedFilter.new source(["foo\n", "bar\n"])
     ary = filt.map {|ev| ev.to_s}
     ary.should == ["foo\n", "bar\n"]
+  end
+  def source(ary)
+    Wolverine::ArraySource.new(ary)
   end
 end
