6b3929df3d6fbf05dc63d51ad1e3d69fbf5a0f05
Marcel M. Cary <marcel@oak.homeunix.org>
2012-07-13 09:18:20 -0700

    Override map, collect, and to_a on an enumerable to return a filterable

--- lib/wolverine/enumerable.rb
+++ lib/wolverine/enumerable.rb
@@ -5,5 +5,19 @@ module Wolverine
     def to_strings
       map {|el| el.to_s}
     end
+    def to_a
+      filterize super
+    end
+    def map(*args)
+      filterize super
+    end
+    alias collect map
+
+    protected
+
+    def filterize(arr)
+      arr.extend(FilterDSL)
+      arr
+    end
   end
 end
--- spec/wolverine/enumerable_spec.rb
+++ spec/wolverine/enumerable_spec.rb
@@ -1,11 +1,23 @@
 require File.dirname(__FILE__)+"/../spec_helper"
 describe Wolverine::Enumerable, "to_strings" do
-  it "should convert contents to strings" do
+  subject do
     Class.new(Wolverine::Enumerable) do
       def each(&block)
         [1, 2].each(&block)
       end
-    end.new.to_strings.should == ["1", "2"]
+    end.new
+  end
+  it "should convert contents to strings" do
+    subject.to_strings.should == ["1", "2"]
+  end
+  it "should allow filters to be applied to #map result" do
+    subject.map {|x| x}.should respond_to(:append_indented)
+  end
+  it "should allow filters to be applied to #collect result" do
+    subject.collect {|x| x}.should respond_to(:append_indented)
+  end
+  it "should allow filters to be applied to #to_a result" do
+    subject.to_a.should respond_to(:append_indented)
   end
 end
 
